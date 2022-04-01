71e4a036ec4d63c2e83366e5d134654c312c27e1
Gautam Sinha <gautam.sinha@live.com>
2022-04-01 16:48:06 -0400

    styles

---
+++ index.js
@@ -0,0 +1,13 @@
+let div = document.getElementById('container')
+console.log('Div', div)
+const getAllStations = async () => {
+    let req = await fetch('https://mtaapi.herokuapp.com/stations')
+    let res = await req.json() 
+    res.result.forEach((element) => {
+        // console.log('Station', element)
+        let h2 = document.createElement('h2')
+        h2.innerText = element.name
+        div.append(h2)
+    })
+}
+getAllStations()

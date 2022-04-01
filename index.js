20a0f16928d27173a1691d52f300dffc0cb23456
Gautam Sinha <gautam.sinha@live.com>
2022-04-01 17:18:54 -0400

    datasets

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const getAllStations = async () => {
         // console.log('Station', element)
         let h2 = document.createElement('h2')
         h2.innerText = element.name
+        h2.dataset.stationId = element.id
+        h2.addEventListener('click', () => {
+            getArrivalTimes(h2.dataset.stationId)
+        })
         div.append(h2)
     })
 }
+
+const getArrivalTimes = async (stationId) => {
+    let req = await fetch(`https://mtaapi.herokuapp.com/api?id=${stationId}`)
+    let res = await req.json() 
+    console.log(res.result.arrivals)
+}
+
 getAllStations()
